0c91f9b7d7632337a615e27c25c377c6b9487a01
Nikita Ryabinin <nikky00r@gmail.com>
2020-01-11 01:01:33 +0300

    #Calc-1  Основной функционал

---
+++ Radar/Radar/Autorization.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Radar
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+     
+        private void AuthButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginTB.Text == "admin" && PasswordTB.Text == "12456")
+            {
+                MessageBox.Show("Zdes' mogla bit' vasha reklama");
+            }
+        }
+    }
+}
